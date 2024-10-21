79e012b7c093120f45af2e69eeac34bc3b19a63d
Roberto Andino <andinorobert518@yahoo.com>
2024-10-21 17:21:01 -0400

    Updating ProductPage

--- src/frontend/components/ProductPage.js
+++ src/frontend/components/ProductPage.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import img1 from '../../images/jonny-gios-X7UFvBn0vZc-unsplash.jpg'
+import img2 from '../../images/jonny-gios-xo0KQrjYkuU-unsplash.jpg'
+import img3 from '../../images/jonny-gios-zDo2oePRJrM-unsplash.jpg'
+import img4 from '../../images/karsten-winegeart-v7aIztpSjxw-unsplash.jpg'
 
 const ProductPage = () => {
+
+    const [selectedImage, setSelectedImage] = useState(
+        "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0"
+    );
+
+    const images = [ img1, img2, img3, img4];
+
     return (
         <div className="container mx-auto p-4">
             <div className="flex flex-col md:flex-row">
                 <div className="w-full md:w-1/2 p-4">
                     <img
                         className="w-full h-auto object-cover rounded-lg"
-                        src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0"
+                        src={selectedImage}
                         alt="Product"
                     />
+                    <div className="flex mt-2 space-x-2">
+                        {images.map((image, index) => (
+                            <img
+                                key={index}
+                                className="w-16 h-16 object-cover cursor-pointer border-2 border-transparent hover:border-gray-300 rounded-lg"
+                                src={image}
+                                alt={`Thumbnail ${index + 1}`}
+                                onClick={() => setSelectedImage(image)}
+                            />
+                        ))}
+                    </div>
                 </div>
             
                 <div className="w-full md:w-1/2 p-4">
