27db3478e08f84d087f8c7effe573b38ea98e6e4
Roberto Andino <andinorobert518@yahoo.com>
2024-11-11 15:12:11 -0500

    Changed yellow to gray.

--- src/frontend/components/Automobiles.js
+++ src/frontend/components/Automobiles.js
@@ -15,21 +15,21 @@ const Automobiles = () => {
     ];
 
     return (
-        <div className="container mx-auto p-8 mt-40 max-w-3xl bg-gray-900 bg-opacity-60 rounded-2xl shadow-xl text-white">
+        <div className="container mx-auto p-8 mt-40 max-w-3xl bg-gray-900 bg-opacity-60 rounded-2xl shadow-xl text-gray-200">
             {images.map((image, index) => (
                 <div key={index} className="flex flex-col items-center mb-16 transition-tranform duration-500 ease-in-out">
-                    <h2 className="text-3xl font-serif text-yellow-300 mb-4">{descriptions[index].title}</h2>
-                    <p className="text-yellow-400 text-lg mb-2">{descriptions[index].info}</p>
+                    <h2 className="text-3xl font-serif text-gray-300 mb-4">{descriptions[index].title}</h2>
+                    <p className="text-lg text-gray-400 mb-2">{descriptions[index].info}</p>
                     <p className="text-gray-300 text-center mb-6 max-w-xl">{descriptions[index].info}</p>
                     <img
-                        className="w-full h-auto object-cover rounded-lg shadow-lg transition-all duration-500"
+                        className="w-full h-auto object-cover rounded-lg shadow-md border-gray-500"
                         src={image}
                         alt={`Classic Car ${index + 1}`}
                     />
                 </div>
             ))}
-            <div className="text-center mt-10">
-                <h3 className="text-2xl mb-4">Interested? Contact Us</h3>
+            <div className="text-center mt-10 border-t border-gray-700 pt-6">
+                <h3 className="text-2xl mb-4 text-gray-300">Interested? Contact Us</h3>
                 <p className="text-lg text-gray-400">📞 (555) 123-4567 | 📧 andinorobert518@yahoo.com</p>
             </div>
         </div>
